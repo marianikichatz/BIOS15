6e47080b3ebf3bf9a5841f6551fc9009e97afdc5
Maria Niki <marianiki@Marias-MacBook-Air.local>
2025-12-14 13:54:58 +0100

    mid

--- midterm/midterm.R
+++ midterm/midterm.R
@@ -1,9 +1,12 @@
 install.packages("glmmTMB")
+install.packages("performance")
+library(performance)
 library(glmmTMB)
 library(car)
 library(tidyverse)
 
 data = read.csv(file = "midterm/exam2023_data-2.csv")
+data <- na.omit(data)
 
 head(data)
 summary(data)
@@ -36,14 +39,6 @@ data$shrub_cover =
 
 data$fern_cover = data$NativePerennialFern_cover
 
-m_cover1 = glmmTMB(Eucalyptus_seedlings ~ grass_cover + graminoid_cover + herb_cover + fern_cover + shrub_cover + Euc_canopy_cover + (1 | Property),family = nbinom1, data = data)
-m_cover2 = glmmTMB(Eucalyptus_seedlings ~ grass_cover + graminoid_cover + herb_cover + fern_cover + shrub_cover + Euc_canopy_cover + (1 | Property),family = nbinom2, data = data)
-AIC(m_cover1,m_cover2)
-
-vif_cover = lm(Eucalyptus_seedlings ~ grass_cover + graminoid_cover + herb_cover + fern_cover + shrub_cover + Euc_canopy_cover, data = data)
-
-summary(m_cover1)
-
 # diff life circles
 
 data$annual_cover = data$ExoticAnnualGrass_cover + data$ExoticAnnualHerb_cover
@@ -55,14 +50,6 @@ data$perennial_cover = data$ExoticPerennialGrass_cover + data$ExoticPerennialHer
   data$NativePerennialFern_cover +
   data$ExoticShrub_cover +
   data$NativeShrub_cover
-
-m_life1 = glmmTMB(Eucalyptus_seedlings ~ annual_cover + perennial_cover + Euc_canopy_cover + (1 | Property), family = nbinom1, data = data)
-m_life2 = glmmTMB(Eucalyptus_seedlings ~ annual_cover + perennial_cover + Euc_canopy_cover + (1 | Property), family = nbinom2, data = data)
-AIC(m_life1, m_life2)
- 
-vif_life = lm(Eucalyptus_seedlings ~ annual_cover + perennial_cover + Euc_canopy_cover, data = data)
-
-summary(m_life1)
 
 # diff origin
 
@@ -78,14 +65,6 @@ data$exotic_cover = data$ExoticAnnualGrass_cover +
   data$ExoticPerennialHerb_cover +
   data$ExoticShrub_cover
 
-m_where1 = glmmTMB(Eucalyptus_seedlings ~ native_cover + exotic_cover + Euc_canopy_cover + (1 | Property),family = nbinom1, data = data)
-m_where2 = glmmTMB(Eucalyptus_seedlings ~ native_cover + exotic_cover + Euc_canopy_cover + (1 | Property),family = nbinom2, data = data)
-AIC(m_where1, m_where2)
-
-vif_where = lm(Eucalyptus_seedlings ~ native_cover + exotic_cover + Euc_canopy_cover, data = data)
-
-summary(m_where1)
-
 # diff origin and type
 
 data$native_grass = data$NativePerennialGrass_cover + data$NativePerennialGraminoid_cover
@@ -97,21 +76,62 @@ data$exotic_grass = data$ExoticAnnualGrass_cover + data$ExoticPerennialGrass_cov
 data$exotic_herb  = data$ExoticAnnualHerb_cover + data$ExoticPerennialHerb_cover
 data$exotic_shrub = data$ExoticShrub_cover
 
+# competition between seedlings and cover from all the plants
+
+data$competition = data$grass_cover + data$graminoid_cover + data$herb_cover + data$fern_cover + data$shrub_cover
+
+# z-score
+num_vars = c("grass_cover", "graminoid_cover", "herb_cover", "fern_cover",
+              "shrub_cover", "Euc_canopy_cover", 
+              "annual_cover", "perennial_cover",
+              "native_cover", "exotic_cover",
+              "native_grass", "native_herb", "native_fern", "native_shrub",
+              "exotic_grass", "exotic_herb", "exotic_shrub",
+              "competition")
+
+data_scaled = data %>%
+  mutate(across(all_of(num_vars), ~ scale(.)[,1]))
+
+# models
+# model cover
+m_cover1 = glmmTMB(Eucalyptus_seedlings ~ grass_cover + graminoid_cover + herb_cover + fern_cover + shrub_cover + Euc_canopy_cover + (1 | Property),family = nbinom1, data = data)
+m_cover2 = glmmTMB(Eucalyptus_seedlings ~ grass_cover + graminoid_cover + herb_cover + fern_cover + shrub_cover + Euc_canopy_cover + (1 | Property),family = nbinom2, data = data)
+AIC(m_cover1,m_cover2)
+
+
+vif_cover = check_collinearity(m_cover1)
+
+summary(m_cover1)
+
+# model life
+m_life1 = glmmTMB(Eucalyptus_seedlings ~ annual_cover + perennial_cover + Euc_canopy_cover + (1 | Property), family = nbinom1, data = data)
+m_life2 = glmmTMB(Eucalyptus_seedlings ~ annual_cover + perennial_cover + Euc_canopy_cover + (1 | Property), family = nbinom2, data = data)
+AIC(m_life1, m_life2)
+ 
+vif_life = check_collinearity(m_life1)
+
+summary(m_life1)
+
+# model where
+m_where1 = glmmTMB(Eucalyptus_seedlings ~ native_cover + exotic_cover + Euc_canopy_cover + (1 | Property),family = nbinom1, data = data)
+m_where2 = glmmTMB(Eucalyptus_seedlings ~ native_cover + exotic_cover + Euc_canopy_cover + (1 | Property),family = nbinom2, data = data)
+AIC(m_where1, m_where2)
+
+vif_where = check_collinearity(m_where1)
+
+summary(m_where1)
+
+# model boss 
 m_boss1 = glmmTMB(Eucalyptus_seedlings ~ native_grass + native_herb + native_fern + native_shrub +
   exotic_grass + exotic_herb + exotic_shrub + Euc_canopy_cover + (1 | Property), family = nbinom1, data = data)
 m_boss2 = glmmTMB(Eucalyptus_seedlings ~ native_grass + native_herb + native_fern + native_shrub +
   exotic_grass + exotic_herb + exotic_shrub + Euc_canopy_cover + (1 | Property), family = nbinom2, data = data)
 AIC(m_boss1,m_boss2)
 
-vif_boss = lm(Eucalyptus_seedlings ~ native_grass + native_herb + native_fern + native_shrub +
-  exotic_grass + exotic_herb + exotic_shrub + Euc_canopy_cover, data = data)
-
+vif_boss = check_collinearity(m_boss1)
 summary(m_boss1)
 
-# competition between seedlings and cover from all the plants
-
-data$competition = data$grass_cover + data$graminoid_cover + data$herb_cover + data$fern_cover + data$shrub_cover
-
+# model competition 
 m_competion1 = glmmTMB(Eucalyptus_seedlings ~ competition + (1 | Property),family = nbinom1,data = data)
 m_competion2 = glmmTMB(Eucalyptus_seedlings ~ competition + (1 | Property),family = nbinom2,data = data)
 AIC(m_competion1,m_competion2)
@@ -120,25 +140,37 @@ summary(m_competion1)
 
 # check for vif
 
-vif(vif_cover)
-vif(vif_life)
-vif(vif_where)
-vif(vif_boss)
+vif_cover
+vif_life
+vif_where
+vif_boss
 
 # Plots
 
 data_long_cover <- data %>%
   pivot_longer(cols = c(grass_cover, herb_cover, fern_cover, shrub_cover), names_to = "plant_type", values_to = "cover")
 
-ggplot(data_long_cover, aes(x = cover, 
-                      y = Eucalyptus_seedlings, 
-                      color = plant_type)) +
-  geom_point(alpha = 0.6) +
-  geom_smooth(method = "loess", se = FALSE, size = 1.1) +
-  labs(
-    x = "Plant cover (%)",
-    y = "Eucalyptus seedlings abundance",
-    ccolor = "Plant type",
-    title = "Relationship between plant cover and Eucalyptus seedlings"
-  ) +
-  theme_minimal(base_size = 14)
+ggplot(data_long_cover, aes(x = plant_type, y = cover, color = plant_type)) +
+  geom_jitter(width = 0.2, alpha = 0.7, size = 2) +  
+  ylab("Cover (%)") +
+  xlab("Plant type") +
+  theme_minimal() +
+  scale_color_brewer(palette = "Set2") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(data_long_cover, aes(x = plant_type, y = cover, fill = plant_type)) +
+  geom_boxplot(alpha = 0.7) +       
+  xlab("Plant type") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set2") +
+   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(data_long_cover %>% filter(plant_type == "grass_cover"), 
+       aes(x = cover, y = Eucalyptus_seedlings)) +
+  geom_point(color = "darkgreen", alpha = 0.7) +
+  geom_smooth(method = "lm", color = "darkgreen") +  
+  ylab("Eucalyptus seedlings") +
+  xlab("Grass cover") +
+  ggtitle("Relationship between Grass cover and Eucalyptus seedlings") +
+  theme_minimal()
+
